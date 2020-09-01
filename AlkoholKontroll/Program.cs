5f4597ce3fd4beeca5b4322f988ef29caa017de1
Felbob <70311872+Felbob@users.noreply.github.com>
2020-09-01 11:09:38 +0200

    Created CanIBuyBeer metod

--- AlkoholKontroll/Program.cs
+++ AlkoholKontroll/Program.cs
@@ -6,8 +6,27 @@ namespace AlkoholKontroll
 {
     class Program
     {
+        static bool CanIBuyBeer(string legg, decimal promille, int age, string location)
+        {
+            var canIBuyBeer = false;
+
+            if (legg == "j" && promille < 1.0m)
+            {
+                if (age > 17 && location == "krogen")
+                    canIBuyBeer = true;
+                else if (age > 19 && location == "systemet")
+                    canIBuyBeer = true;
+                else
+                    canIBuyBeer = false;
+            }
+
+            return canIBuyBeer;
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Ver 1.01");
+            Console.WriteLine();
+            
             Console.Write("Mata in din ålder: ");
             var age = Convert.ToInt32(Console.ReadLine());
 
@@ -20,15 +39,7 @@ namespace AlkoholKontroll
             Console.Write("Vilken promille halt har du? ");
             var promille = Convert.ToDecimal(Console.ReadLine());
 
-            var canIBuyBeer = false;
-
-            if (legg == "j" && promille < 1.0m)
-            {
-                if (age > 17 && location == "krogen")
-                    canIBuyBeer = true;
-                else if (age > 19 && location == "systemet")
-                    canIBuyBeer = true;
-            }
+            var canIBuyBeer = CanIBuyBeer(legg, promille, age, location);
 
             if (canIBuyBeer)
                 Console.WriteLine("Du får köpa öl!");
